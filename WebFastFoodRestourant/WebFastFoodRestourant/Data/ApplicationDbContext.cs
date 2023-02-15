ce0184b4bad5eb03843bc2a2d444eeaed2348f40
YOANAI <116347326+YOANAI@users.noreply.github.com>
2023-02-15 10:17:58 +0200

    add

---
+++ WebFastFoodRestourant/WebFastFoodRestourant/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebFastFoodRestaurantApp.Domain;
+
+namespace WebFastFoodRestourant.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> PrOrdersoducts { get; set; }
+    }
+}
---
+++ WebFastFoodRestourant/WebFastFoodRestourant/Domain/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFastFoodRestaurantApp.Domain
+{
+    public class Product
+    {
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string ProductName { get; set; }
+        [Required]
+        public int BrandId { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public string Picture { get; set; }
+        [Required]
+        [Range(0, 5000)]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public virtual IEnumerable<Order> Orders { get; set; } = new List<Order>();
+    }
+}
